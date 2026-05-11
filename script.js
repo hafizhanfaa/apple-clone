86458c63af9bce8081b4d5c90a1e30f29a2aef8d
hafizhanfaa <hafizhanfa2010@gmail.com>
2026-05-11 09:35:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const items = document.querySelectorAll(".faq-item");
+
+items.forEach(item => {
+  const button = item .querySelector(".faq-question");
+
+  button.addEventListener("click" , () => {;
+  
+
+  item.classList.toggle("active");
+  });
+});
